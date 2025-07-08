c54cb4fd9e82df1f88b15f90200b0e3f698d633c
Creator Rama <ap410485@gmail.com>
2025-07-08 18:09:50 +0530

    added url of frontend in cors and socket.io

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,13 @@ connectToDatabase()
 const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","http://192.168.57.161:5173"],
+    origin: ["http://localhost:5173","http://192.168.57.161:5173","https://collab-todo-dusky.vercel.app"],
     methods: ["GET", "POST"]
   }
 });
 // Middleware
 app.use(cors({
-  origin:["http://192.168.57.161:5173","http://localhost:5173"]
+  origin:["http://192.168.57.161:5173","http://localhost:5173","https://collab-todo-dusky.vercel.app"]
 }));
 app.use(express.json());
 
